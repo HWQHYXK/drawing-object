2914204e70db7f0398c4da65490b42d143685042
HWQ <941822900@qq.com>
2019-01-04 00:59:10 +0800

    add MyChooser

---
+++ src/drawingBoard/MyChooser.java
@@ -0,0 +1,22 @@
+package drawingBoard;
+
+import javafx.scene.input.MouseEvent;
+
+public class MyChooser implements Tool
+{
+    @Override
+    public void press(MouseEvent e, Board pane)
+    {
+
+    }
+    @Override
+    public void drag(MouseEvent e, Board pane)
+    {
+
+    }
+    @Override
+    public void release(MouseEvent e, Board pane)
+    {
+
+    }
+}
