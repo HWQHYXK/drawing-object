5f0efbac8ed83bf61eed0345bdd6dce21d77cd4e
HWQ <941822900@qq.com>
2019-01-04 01:20:20 +0800

    add PropertyBar

--- src/drawingBoard/MainPane.java
+++ src/drawingBoard/MainPane.java
@@ -3,24 +3,28 @@ package drawingBoard;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MainPane extends BorderPane
 {
     private MenuBar top;
     private Board center;
-    private ToolBar left;
     private Cue bottom;
+    private ToolBar left;
+    private PropertyBar right;
     public MainPane()
     {
         center=new Board(this);
         left=new ToolBar(this);
         top = new drawingBoard.MenuBar(this);
         bottom = new Cue(this);
+        right =new PropertyBar(this);
 
-        setCenter(center);//中间的画板
-        setLeft(left);//左边的工具栏
         setTop(top);//上面的菜单
-        setBottom(bottom);
+        setCenter(center);//中间的画板
+        setBottom(bottom);//下面的坐标提示栏
+        setLeft(left);//左边的工具栏
+        setRight(right);//右边的属性栏
     }
     public MenuBar getMyTop()
     {
@@ -37,5 +41,9 @@ public class MainPane extends BorderPane
     public HBox getMyBottom()
     {
         return bottom;
+    }
+    public VBox getMyRight()
+    {
+        return right;
     }
 }
---
+++ src/drawingBoard/PropertyBar.java
@@ -0,0 +1,13 @@
+package drawingBoard;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class PropertyBar extends VBox
+{
+    BorderPane fa;
+    public PropertyBar(BorderPane fa)
+    {
+        this.fa=fa;
+    }
+}
