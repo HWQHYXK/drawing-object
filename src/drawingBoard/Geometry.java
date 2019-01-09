7073f20e1a4540caa601281c4eeea5fdff15ddcd
HWQ <941822900@qq.com>
2019-01-09 17:26:36 +0800

    finally OK

--- src/drawingBoard/Geometry.java
+++ src/drawingBoard/Geometry.java
@@ -17,16 +17,24 @@ public class Geometry
             x=xx;y=yy;
         }
     }
+    static  double getCenterX(Node node)
+    {
+        return (node.getLayoutBounds().getMaxX()+node.getLayoutBounds().getMinX())/2;
+    }
+    static  double getCenterY(Node node)
+    {
+        return (node.getLayoutBounds().getMaxY()+node.getLayoutBounds().getMinY())/2;
+    }
     static boolean inRange(double x1, double y1, double x2, double y2, Node node)
     {
         x1 = x1 - node.getLayoutX();y1 = y1 - node.getLayoutY();
         x2 = x2 - node.getLayoutX();y2 = y2 - node.getLayoutY();
 
         //被包含
-        if(node.contains(x1-node.getScaleX(),y1-node.getScaleY())) return true;
-        if(node.contains(x1-node.getScaleX(),y2-node.getScaleY())) return true;
-        if(node.contains(x2-node.getScaleX(),y1-node.getScaleY())) return true;
-        if(node.contains(x2-node.getScaleX(),y2-node.getScaleY())) return true;
+        if(contain(node,x1-getCenterX(node),y1-getCenterY(node))) return true;
+        if(contain(node,x1-getCenterX(node),y2-getCenterY(node))) return true;
+        if(contain(node,x2-getCenterX(node),y1-getCenterY(node))) return true;
+        if(contain(node,x2-getCenterX(node),y2-getCenterY(node))) return true;
 
         //相交
         if(intersect(x1,y1,x2,y2,node)) return true;
@@ -36,6 +44,13 @@ public class Geometry
         if(new Rectangle(Math.min(x1,x2),Math.min(y1,y2),Math.abs(x1-x2),Math.abs(y1-y2)).contains(point.x,point.y)) return true;
 
         return false;
+    }
+    static boolean contain(Node node ,double x,double y)
+    {
+        Point A=new Point(x,y);
+        double angle=node.getRotate();
+        Point P=new Point(getCenterX(node),getCenterY(node));
+        return node.contains(rotate(A,P,-angle).x,rotate(A,P,-angle).y);
     }
     static boolean intersect(double x1, double y1, double x2, double y2, Node node)
     {
@@ -56,7 +71,7 @@ public class Geometry
             double endX=X + ((Rectangle) node).getWidth();
             double endY=Y + ((Rectangle) node).getHeight();
 
-            Point P=new Point(node.getScaleX(),node.getScaleY());
+            Point P=new Point(getCenterX(node),getCenterY(node));
             if(intersect(x1,y1,x2,y2,rotate(new Line(X,Y,X,endY),P,angle))) return true;
             if(intersect(x1,y1,x2,y2,rotate(new Line(X,endY,endX,endY),P,angle))) return true;
             if(intersect(x1,y1,x2,y2,rotate(new Line(endX,endY,endX,Y),P,angle))) return true;
@@ -68,7 +83,7 @@ public class Geometry
         {
             List<Double> a=((Polyline)node).getPoints();
 
-            Point P=new Point(node.getScaleX(),node.getScaleY());
+            Point P=new Point(getCenterX(node),getCenterY(node));
             if(intersect(x1,y1,x2,y2,rotate(new Line(a.get(a.size()-2),a.get(a.size()-1),a.get(0),a.get(1)),P,angle))) return true;
             for(int i=0;i+3<a.size();i+=2)
                 if(intersect(x1,y1,x2,y2,rotate(new Line(a.get(i),a.get(i+1),a.get(i+2),a.get(i+3)),P,angle))) return true;
@@ -77,18 +92,20 @@ public class Geometry
         }
         else if(node instanceof Ellipse)
         {
-            if(Math.abs(((Ellipse) node).getRotate()) >eps)
+            Ellipse ellipse=(Ellipse) node;
+            Point A=new Point(x1,y1),B=new Point(x1,y2);
+            Point C=new Point(x2,y2),D=new Point(x2,y1);
+            if(Math.abs(ellipse.getRotate()) > eps)
             {
-                Ellipse ellipse=(Ellipse) node;
                 ellipse=new Ellipse(ellipse.getCenterX(),ellipse.getCenterY(),ellipse.getRadiusX(),ellipse.getRadiusY());
-                Point P=new Point(ellipse.getScaleX(),ellipse.getScaleY());
-                Point A=rotate(new Point(x1,y1),P,-angle),B=rotate(new Point(x2,y2),P,-angle);
-                return intersect(A.x,A.y,B.x,B.y,ellipse);
+                Point P=new Point(getCenterX(node),getCenterY(node));
+                A=rotate(A,P,-angle);B=rotate(B,P,-angle);
+                C=rotate(C,P,-angle);D=rotate(D,P,-angle);
             }
-            if(intersect(new Line(x1,y1,x1,y2),(Ellipse) node)) return true;
-            if(intersect(new Line(x1,y2,x2,y2),(Ellipse) node)) return true;
-            if(intersect(new Line(x2,y2,x2,y1),(Ellipse) node)) return true;
-            if(intersect(new Line(x2,y1,x1,y1),(Ellipse) node)) return true;
+            if(intersect(new Line(A.x,A.y,B.x,B.y),ellipse)) return true;
+            if(intersect(new Line(B.x,B.y,C.x,C.y),ellipse)) return true;
+            if(intersect(new Line(C.x,C.y,D.x,D.y),ellipse)) return true;
+            if(intersect(new Line(D.x,D.y,A.x,A.y),ellipse)) return true;
 
             return false;
         }
@@ -97,21 +114,23 @@ public class Geometry
     static Point getPoint(Node node)
     {
         //得到node中的一个点
+        double angle=node.getRotate();
+        Point P=new Point(getCenterX(node),getCenterY(node));
         if(node instanceof Line)
         {
-            return new Point(((Line)node).getStartX(),((Line)node).getStartY());
+            return rotate(new Point(((Line)node).getStartX(),((Line)node).getStartY()),P,angle);
         }
         else if(node instanceof Rectangle)
         {
-            return new Point(((Rectangle)node).getX(),((Rectangle)node).getY());
+            return rotate(new Point(((Rectangle)node).getX(),((Rectangle)node).getY()),P,angle);
         }
         else if(node instanceof  Polyline)
         {
-            return new Point(((Polyline)node).getPoints().get(0),((Polyline)node).getPoints().get(1));
+            return rotate(new Point(((Polyline)node).getPoints().get(0),((Polyline)node).getPoints().get(1)),P,angle);
         }
         else if(node instanceof  Ellipse)
         {
-            return new Point(node.getScaleX(),node.getScaleY());
+            return new Point(getCenterX(node),getCenterY(node));
         }
         return null;
     }
@@ -119,22 +138,16 @@ public class Geometry
     {
         //A绕着B转angle
         //angle在此处才转化为弧度，其他地方用的都是角度
-        angle=-angle/180*Math.PI;
+        angle=angle/180*Math.PI;
         double dx=A.x-B.x,dy=A.y-B.y;
-        return new Point(dx*Math.cos(angle)-dy*Math.sin(angle)+B.x,dy*Math.cos(angle)-dx*Math.sin(angle)+B.y);
-    }
-    static void rotate(Line l)
-    {
-        //把getrotate()值实际旋转
-        if(Math.abs(l.getRotate()) > eps)
-            l=rotate(l,l.getRotate());
+        return new Point(dx*Math.cos(angle)-dy*Math.sin(angle)+B.x,dy*Math.cos(angle)+dx*Math.sin(angle)+B.y);
     }
     static Line rotate(Line l,double angle)
     {
         //l自转angle
         Point A=new Point(l.getStartX(),l.getStartY());
         Point B=new Point(l.getEndX(),l.getEndY());
-        Point P=new Point((l.getStartX()+l.getEndX())/2,(l.getStartY()+l.getEndY())/2);
+        Point P=new Point(getCenterX(l),getCenterY(l));
         return new Line(rotate(A,P,angle).x,rotate(A,P,angle).y,rotate(B,P,angle).x,rotate(B,P,angle).y);
     }
     static Line rotate(Line l,Point P,double angle)
@@ -147,7 +160,7 @@ public class Geometry
     static boolean left(double x,double y,Line l)
     {
         //点(x,y)是否在线段l的左边
-        rotate(l);
+        l=rotate(l,l.getRotate());
         double x1=l.getEndX()-l.getStartX();
         double y1=l.getEndY()-l.getStartY();
         double x2=x-l.getStartX();
@@ -157,7 +170,7 @@ public class Geometry
     static boolean intersect(Line l1,Line l2)
     {
         //线段l1和l2是否相交
-        rotate(l1);rotate(l2);
+        l1=rotate(l1,l1.getRotate());l2=rotate(l2,l2.getRotate());
         if(left(l1.getStartX(),l1.getStartY(),l2) == left(l1.getEndX(),l1.getEndY(),l2)) return false;
         if(left(l2.getStartX(),l2.getStartY(),l1) == left(l2.getEndX(),l2.getEndY(),l1)) return false;
         return true;
@@ -165,7 +178,7 @@ public class Geometry
     static boolean intersect(Line l,Ellipse ellipse)
     {
         //线段l和椭圆ellipse是否相交
-        rotate(l);
+        l=rotate(l,l.getRotate());
         Point A=new Point(l.getStartX(),l.getStartY());
         Point B=new Point(l.getEndX(),l.getEndY());
         Boolean containsA,containsB;
@@ -185,11 +198,11 @@ public class Geometry
         }
         double k=(A.y-B.y)/(A.x-B.x);
         double t=A.x*(A.y-B.y)/(B.x-A.x)+A.y;
-        double delta=a*a*a*a*b*b*k*k-(a*a*k*k+b*b)*(a*a*t*t-a*a*b*b);
+        double delta=a*a*a*a*t*t*k*k-(a*a*k*k+b*b)*(a*a*t*t-a*a*b*b);
         if(delta < -eps) return false;
-        double x1=(-2*a*a*b*k-Math.sqrt(delta))/(2*(a*a*k*k+b*b));
+        double x1=(-2*a*a*t*k-Math.sqrt(delta))/(2*(a*a*k*k+b*b));
         if(x1<Math.min(A.x,B.x) || x1>Math.max(A.x,B.x)) return false;
-        double x2=(-2*a*a*b*k+Math.sqrt(delta))/(2*(a*a*k*k+b*b));
+        double x2=(-2*a*a*t*k+Math.sqrt(delta))/(2*(a*a*k*k+b*b));
         if(x2<Math.min(A.x,B.x) || x2>Math.max(A.x,B.x)) return false;
         return true;
     }
